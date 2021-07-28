a580fd9a359de579bdec184c42a2a6a10c320e8a
Pszione <oused.games@gmail.com>
2021-07-28 10:25:38 -0300

    Responsive Website - Fix naming

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_responsive_website/screens/home/home_screen.dart';
+import 'package:flutter_responsive_website/screens/main/main_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,17 +18,17 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       // we are using dark theme and we modify it as our need
       theme: ThemeData.dark().copyWith(
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: bgColor,
-        canvasColor: bgColor,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBackgroundColor,
+        canvasColor: kBackgroundColor,
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
             .apply(bodyColor: Colors.white)
             .copyWith(
-              bodyText1: TextStyle(color: bodyTextColor),
-              bodyText2: TextStyle(color: bodyTextColor),
+              bodyText1: TextStyle(color: kTextBodyColor),
+              bodyText2: TextStyle(color: kTextBodyColor),
             ),
       ),
-      home: HomeScreen(),
+      home: MainScreen(),
     );
   }
 }
