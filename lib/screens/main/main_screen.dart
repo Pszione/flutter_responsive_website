7aa284610b670b43a06d176f0368c86f261d92d9
Pszione <oused.games@gmail.com>
2021-07-28 10:54:11 -0300

    Responsive Website - SidePanel part I - CircleAvatar

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_website/constants.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints(maxHeight: kMaxWebsiteWidth),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              // Now this take (2+7=9) so 2/9=0.22 means 22%
+              // flex: 2,
+              flex: 3, // 27%
+              child: Drawer(
+                child: Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 1.23,
+                      child: Container(
+                        color: Color(0xFF242430),
+                        child: Column(
+                          children: <Widget>[
+                            const CircleAvatar(
+                              radius: 50,
+                              backgroundImage: AssetImage(
+                                  'assets/images/35244548_pedro_santos.png'),
+                            ),
+                            Text(
+                              'Pedro Santos',
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                            const Text(
+                              'Learning Flutter (by Google) | mobile developer/game developer | \nCurious for the unknown',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w200,
+                                height: 1.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              // Now this take (7/9=0.78) means 78% width
+              // flex: 7,
+              flex: 8,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
