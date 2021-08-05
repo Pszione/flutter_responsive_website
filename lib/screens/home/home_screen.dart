3d62d1455147826f862ecca2954e9db80653c483
Pszione <oused.games@gmail.com>
2021-08-04 23:04:07 -0300

    Responsive Website - User Recommendations | FINISH

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_website/screens/home/projects_grid.dart';
 import 'package:flutter_responsive_website/screens/home/user_info_highlights.dart';
+import 'package:flutter_responsive_website/screens/home/user_recommendations_card.dart';
 import 'package:flutter_responsive_website/screens/main/main_screen.dart';
 
+import '../../constants.dart';
 import 'home_header_banner.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -17,6 +19,7 @@ class HomeScreen extends StatelessWidget {
         // That's it for part I (above)
         const HighlightNumbersIndicators(),
         const UserProjectsGrid(),
+        const UserRecommendations(),
       ],
     );
   }
---
+++ lib/screens/home/user_recommendations_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_website/models/recommendation.dart';
+
+import '../../constants.dart';
+
+class UserRecommendations extends StatelessWidget {
+  const UserRecommendations({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recommendations',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(height: kDefaultPadding),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                demoRecommendations.length,
+                (int index) => Padding(
+                  padding: const EdgeInsets.only(right: kDefaultPadding),
+                  child: RecommendationCard(
+                      recommendation: demoRecommendations[index]),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendationCard extends StatelessWidget {
+  const RecommendationCard({
+    Key? key,
+    required this.recommendation,
+  }) : super(key: key);
+
+  final Recommendation recommendation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      padding: const EdgeInsets.all(kDefaultPadding),
+      color: kTertiaryColor,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            recommendation.name!,
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+          Text(recommendation.source!),
+          const SizedBox(height: kDefaultPadding),
+          Text(
+            recommendation.text!,
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(height: kTextMediumHeightSpacing),
+          ),
+        ],
+      ),
+    );
+  }
+}
