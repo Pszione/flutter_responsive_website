f584c6a4311a662c67e4ca3c320a01f9a7980443
Pszione <oused.games@gmail.com>
2021-08-24 00:10:39 -0300

    Responsive Website -  Animation/404 error screen version 02 part III

--- lib/screens/404/body.dart
+++ lib/screens/404/body.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_website/responsive.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -17,12 +19,46 @@ class _BodyState extends State<Body> {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-        width: double.infinity,
-        height: 600, // MediaQuery.of(context).size.height,
-        //child: _404ErrorScreenVersion01(),
-        child: _404ErrorScreenAnimated_Version02(),
-      ),
+          // padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30), // zero
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height, // 600
+          //child: _404ErrorScreenVersion01(),
+          child: Responsive.isDesktop(context)
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // 404 text
+                        const Center(child: ErrorText()),
+                        // Bicycle
+                        FittedBox(
+                          child: _404ErrorScreenAnimated_Version02(),
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // 404 text
+                        const Center(child: ErrorText()),
+                        // Bicycle
+                        FittedBox(
+                          child: _404ErrorScreenAnimated_Version02(),
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
     );
   }
 }
@@ -38,100 +74,110 @@ class _404ErrorScreenAnimated_Version02 extends StatefulWidget {
 }
 
 class _404ErrorScreenAnimated_Version02State
-    extends State<_404ErrorScreenAnimated_Version02> {
+    extends State<_404ErrorScreenAnimated_Version02>
+    with SingleTickerProviderStateMixin {
   final Color yellow = const Color(0xFFFFB800);
 
   bool startedAnimation = false;
+  late AnimationController _animationController;
   final Duration duration = const Duration(milliseconds: 500);
 
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 250), vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    // Start everything
     Future.delayed(const Duration(milliseconds: 200), () {
       // will trigger our animation after 100 millis
       setState(() {
         startedAnimation = true;
       });
     });
+    // Rotate bike
+    Future.delayed(Duration(milliseconds: duration.inMilliseconds + 200), () {
+      _animationController.forward();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(_animationController.value);
+    //
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // 404 text
-            const Center(child: ErrorText()),
-            // Bicycle
-            SizedBox(
-              width: 641,
-              height: 310,
-              child: Stack(
-                children: [
-                  //
-                  //
-                  // Shadow
-                  AnimatedPositioned(
-                    top: 0,
-                    left: startedAnimation ? 0 : 50,
-                    duration: duration,
-                    child: AnimatedOpacity(
-                      opacity: startedAnimation ? 1 : 0.3,
-                      duration: duration,
-                      child: SvgPicture.asset(
-                        'assets/icons/_404/Group 64.svg',
-                        height: 260.3,
-                        color: yellow,
-                      ),
-                    ),
+        SizedBox(
+          width: 641,
+          height: 310,
+          child: Stack(
+            children: [
+              //
+              //
+              // Shadow
+              AnimatedPositioned(
+                top: 0,
+                left: startedAnimation ? 0 : 50,
+                duration: duration,
+                child: AnimatedOpacity(
+                  opacity: startedAnimation ? 1 : 0.3,
+                  duration: duration,
+                  child: SvgPicture.asset(
+                    'assets/icons/_404/Group 64.svg',
+                    height: 260.3,
+                    color: yellow,
                   ),
-                  //
-                  //
-                  // Cycle
-                  AnimatedPositioned(
-                    bottom: 0,
-                    left: startedAnimation ? 130 : 0, // opposite of the shadow
-                    duration: duration,
-                    child: SizedBox(
-                      height: 310,
-                      width: 528,
-                      child: Stack(
-                        overflow: Overflow.visible,
-                        children: [
-                          Positioned(
-                            bottom: 7,
-                            left: 230,
-                            child: SvgPicture.asset(
-                                'assets/icons/_404/cycle_part_2.svg',
-                                height: 142),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            // left: 120,
-                            child: SvgPicture.asset(
-                                'assets/icons/_404/cycle_part_1.svg',
-                                height: 287),
-                          ),
-                          Positioned(
-                            left: 0,
-                            bottom: 0,
-                            child: SvgPicture.asset(
-                              'assets/icons/_404/line.svg',
-                              height: 6.4,
-                              color: yellow,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+              //
+              //
+              // Cycle
+              AnimatedPositioned(
+                bottom: 0,
+                left: startedAnimation ? 130 : 0, // opposite of the shadow
+                duration: duration,
+                child: SizedBox(
+                  height: 310,
+                  width: 528,
+                  child: Stack(
+                    overflow: Overflow.visible,
+                    children: [
+                      Positioned(
+                        bottom: 7,
+                        left: 230 + (_animationController.value * 90),
+                        child: SvgPicture.asset(
+                            'assets/icons/_404/cycle_part_2.svg',
+                            height: 142),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        // left: 120,
+                        child: Transform.rotate(
+                          angle: (_animationController.value * 15) * (pi / 180),
+                          child: SvgPicture.asset(
+                              'assets/icons/_404/cycle_part_1.svg',
+                              height: 287),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        bottom: 0,
+                        child: SvgPicture.asset(
+                          'assets/icons/_404/line.svg',
+                          height: 6.4,
+                          color: yellow,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
